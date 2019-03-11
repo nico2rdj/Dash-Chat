564344b84a79aaf7f33e998072f4abb9430cbc8b
nico2rdj <nico2rdj@github.com>
2019-03-11 14:39:15 +0100

    Firebase + chat

--- Fire.js
+++ Fire.js
@@ -2,6 +2,7 @@ import firebase from "firebase";
 class Fire {
   constructor() {
     this.init();
+    this.observeAuth();
   }
 
   init = () => {
@@ -16,4 +17,88 @@ class Fire {
     };
     firebase.initializeApp(config);
   };
+
+  // check si on est connecté
+  observeAuth = () =>
+    firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
+
+  // appel pour la connexion
+  onAuthStateChanged = user => {
+    if (!user) {
+      try {
+        // si on était pas connecté nous connecte en mode anonyme
+        firebase.auth().signInAnonymously();
+      } catch ({ message }) {
+        alert(message);
+      }
+    }
+  };
+
+  // retourne de la route messages dans la db
+  get ref() {
+    return firebase.database().ref("messages");
+  }
+
+  // recupere les 20 derniers messages de la /messages et dès qu'il y a un nouveau
+  // message ajouté on le récupère et on le passe a la methode parse
+  on = callback =>
+    this.ref
+      .limitToLast(20)
+      .on("child_added", snapshot => callback(this.parse(snapshot)));
+
+  // traitement du nouveau message
+  parse = snapshot => {
+    // recupere les valeurs du snapshot contenant le nouveau message
+    // pour les mettres dans le format de giftedchat
+    const { timestamp: numberStamp, text, user } = snapshot.val();
+    const { key: _id } = snapshot;
+
+    const timestamp = new Date(numberStamp);
+    // message dans le format de giftedchat
+    const message = {
+      _id,
+      timestamp,
+      text,
+      user
+    };
+    return message;
+  };
+
+  // Se deconnecté de la db
+  off() {
+    this.ref.off();
+  }
+
+  // recupere l'id de l'utilisateur connecté
+  get uid() {
+    return (firebase.auth().currentUser || {}).uid;
+  }
+
+  // recupere l'heure sur le serveur
+  get timestamp() {
+    return firebase.database.ServerValue.TIMESTAMP;
+  }
+
+  // loop sur un tableau de messages
+  // on les mets au bon format
+  // on les push sur le serveur
+  send = messages => {
+    for (let i = 0; i < messages.length; i++) {
+      const { text, user } = messages[i];
+
+      // 4.
+      const message = {
+        text,
+        user,
+        timestamp: this.timestamp
+      };
+      this.append(message);
+    }
+  };
+
+  // ajout de message dans la db
+  append = message => this.ref.push(message);
 }
+
+Fire.shared = new Fire();
+export default Fire;
--- components/ChatComponent.js
+++ components/ChatComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { View, StyleSheet } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
+import Fire from "../Fire";
 
 class Chat extends React.Component {
   static navigationOptions = ({ navigation }) => ({
@@ -9,8 +10,38 @@ class Chat extends React.Component {
   state = {
     messages: []
   };
+
+  // on let sur écoute le serveur dès qu'il y a un changement
+  // on réactualise le store de message
+  componentDidMount() {
+    Fire.shared.on(message =>
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message)
+      }))
+    );
+  }
+
+  // quand on quitte le channel on coupe l'écoute
+  componentWillUnmount() {
+    Fire.shared.off();
+  }
+
+  get user() {
+    // Return our name and our UID for GiftedChat to parse
+    return {
+      name: this.props.navigation.state.params.name,
+      _id: Fire.shared.uid
+    };
+  }
+
   render() {
-    return <GiftedChat messages={this.state.message} />;
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={Fire.shared.send}
+        user={this.user}
+      />
+    );
   }
 }
 
